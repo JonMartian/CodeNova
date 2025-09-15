9c4642db0d770074b1e2aed81b282fe9cdc28a40
Andres Javier <martian@MacBook-Pro.local>
2025-09-15 19:42:47 +0800

    Merge remote main into local main

---
+++ test.js
@@ -0,0 +1,11 @@
+function myFunction(p1,p2){
+    return p1 * p2
+}
+
+let myFunction2 = function(a,b) {return a * b}
+
+let myFunction3 = (a, b) => a * b
+
+function myFunction() {
+    let carName = "Volvo"
+}
